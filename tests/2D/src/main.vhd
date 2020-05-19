c36643f2d84551cd7a0d18dc6fa09ea371ee0208
bilibili <fenggaobuke@gmail.com>
2020-05-19 14:08:05 +0800

    add test demo for 2D renderer

--- tests/2D/src/main.vhd
+++ tests/2D/src/main.vhd
@@ -22,14 +22,31 @@ end entity main; -- main
 
 architecture arch of main is
 
-   --component demo_geom_gen is
-   --   port (
-   --      n_element   : out unsigned(11 downto 0);
-   --      geobuf_clk  : in std_logic;
-   --      geobuf_addr : in std_logic_vector(11 downto 0);
-   --      geobuf_q    : out  std_logic_vector(23 downto 0);
-   --   );
-   --end component demo_geom_gen;
+   component geometry_demo is
+      port (
+         -- control signals
+         clk0   : in std_logic;              -- must not exceed max freq of RAM
+         render_start : out std_logic;
+         -- internal ports to geometry input buffer (RAM)
+         n_element   : out unsigned(11 downto 0);
+         geobuf_clk  : out std_logic;
+         geobuf_wren : out std_logic;
+         geobuf_addr : out std_logic_vector(11 downto 0);
+         geobuf_data : out std_logic_vector(23 downto 0)
+      );
+   end component geometry_demo;
+
+   component geometry_buffer_ram is
+      port (
+         data        : IN STD_LOGIC_VECTOR (23 DOWNTO 0);
+         rdaddress   : IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+         rdclock     : IN STD_LOGIC ;
+         wraddress   : IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+         wrclock     : IN STD_LOGIC  := '1';
+         wren        : IN STD_LOGIC  := '0';
+         q           : OUT STD_LOGIC_VECTOR (23 DOWNTO 0)
+      );
+   end component geometry_buffer_ram;
 
    component renderer2d is
       port(
@@ -110,6 +127,17 @@ architecture arch of main is
    -- pll output to sram
 	signal srampulse: std_logic;
 
+   signal render_n_element: unsigned(11 downto 0);
+   signal render_start : std_logic;
+   -- ports of geometry buffers
+   signal geobuf_in_clk  : std_logic;
+   signal geobuf_in_wren : std_logic;
+   signal geobuf_in_addr : std_logic_vector(11 downto 0);
+   signal geobuf_in_data : std_logic_vector(23 downto 0);
+   signal geobuf_out_clk  : std_logic;
+   signal geobuf_out_addr : std_logic_vector(11 downto 0);
+   signal geobuf_out_q    : std_logic_vector(23 downto 0);
+
 begin
 
 	pll: main_pll port map (clk0, srampulse);
@@ -121,15 +149,36 @@ begin
    --   end if;
    --end process;
 
+   -- genmetry instantiation module
+   demo: geometry_demo port map (
+      clk0         => clk0,
+      render_start => render_start,
+      n_element    => render_n_element,
+      geobuf_clk   => geobuf_in_clk,
+      geobuf_wren  => geobuf_in_wren,
+      geobuf_addr  => geobuf_in_addr,
+      geobuf_data  => geobuf_in_data
+   );
+
+   geometry_buffer : geometry_buffer_ram port map (
+      data      => geobuf_in_data,
+      rdaddress => geobuf_out_addr,
+      rdclock   => geobuf_out_clk,
+      wraddress => geobuf_in_addr,
+      wrclock   => geobuf_in_clk,
+      wren      => geobuf_in_wren,
+      q         => geobuf_out_q
+   );
+
    renderer: renderer2d port map (
       clk0        => clk0,
-      start       => '1',
+      start       => render_start,
       busy        => open,
       -- geometry info
-      n_element   => to_unsigned(2, 12),
-      geobuf_clk  => open,
-      geobuf_addr => open,
-      geobuf_q    => x"001001",
+      n_element   => render_n_element,
+      geobuf_clk  => geobuf_out_clk,
+      geobuf_addr => geobuf_out_addr,
+      geobuf_q    => geobuf_out_q,
       -- sram ports
       sram_addr1  => mem_addr2,
       sram_q1     => mem_q2,
@@ -164,6 +213,7 @@ begin
       chsl_e   => sram_ce
    );
 
+   -- display control module
    vga: vga_controller port map (
       clk_0    => clk0,
       reset    => '1',
