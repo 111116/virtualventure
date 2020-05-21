c20e939fe0f0b3f20fc5058d4bb46c85d6231c69
bilibili <fenggaobuke@gmail.com>
2020-05-21 23:35:09 +0800

    fixed-prototype: use only elementary operators

---
+++ tests/3D-int16-prototype/src/genmodel.hpp
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <vector>
+#include "data_types.hpp"
+
+
+std::vector<Vertex> genrail(real ox, real oy, real oz)
+{
+	// uv: 30,155 - 426,543
+	// left front
+	Vertex a = {-1 + ox, 0 + oy, 0 + oz, 1,  30, 543};
+	// left back
+	Vertex b = {-1 + ox, 0 + oy, 8 + oz, 1,  30, 155};
+	// right front
+	Vertex c = { 1 + ox, 0 + oy, 0 + oz, 1, 426, 543};
+	// right back
+	Vertex d = { 1 + ox, 0 + oy, 8 + oz, 1, 426, 155};
+	a.u /= 64;
+	a.v /= 64;
+	b.u /= 64;
+	b.v /= 64;
+	c.u /= 64;
+	c.v /= 64;
+	d.u /= 64;
+	d.v /= 64;
+	return {a,b,d,a,d,c};
+}
+
+std::vector<Vertex> gencar(real ox, real oy, real oz)
+{
+	// params
+	real topw = 0.55f; // half top width
+	real botw = 0.8f; // half bottom width
+	real toph = 2.0f; // height of car
+	real both = 0.1f; // distance to ground
+	real len  = 5.0f; // length of car
+	// frontface uv: 50,796  216,796  4,1010  261,1010
+	Vertex a = {-botw + ox, both + oy, 0 + oz, 1,  4, 1010}; // left bottom
+	Vertex b = {-topw + ox, toph + oy, 0 + oz, 1,  50, 796}; // left top
+	Vertex c = { botw + ox, both + oy, 0 + oz, 1, 261, 1010}; // right bottom
+	Vertex d = { topw + ox, toph + oy, 0 + oz, 1, 216, 796}; // right top
+	a.u /= 64; a.v /= 64;
+	b.u /= 64; b.v /= 64;
+	c.u /= 64; c.v /= 64;
+	d.u /= 64; d.v /= 64;
+	// top face uv: 268,759 - 600,814
+	Vertex t1 = {-topw + ox, toph + oy,   0 + oz, 1, 268, 759};
+	Vertex t2 = {-topw + ox, toph + oy, len + oz, 1, 268, 814};
+	Vertex t3 = { topw + ox, toph + oy,   0 + oz, 1, 600, 759};
+	Vertex t4 = { topw + ox, toph + oy, len + oz, 1, 600, 814};
+	t1.u /= 64, t1.v /= 64;
+	t2.u /= 64, t2.v /= 64;
+	t3.u /= 64, t3.v /= 64;
+	t4.u /= 64, t4.v /= 64;
+	// left face uv: 282,821  744,821  268,1009  758,1009
+	Vertex l1 = {-botw + ox, both + oy,   0 + oz, 1, 268, 1009}; // front bot
+	Vertex l2 = {-topw + ox, toph + oy,   0 + oz, 1, 282, 821};  // front top
+	Vertex l3 = {-botw + ox, both + oy, len + oz, 1, 758, 1009}; // far bot
+	Vertex l4 = {-topw + ox, toph + oy, len + oz, 1, 744, 821};  // far top
+	l1.u /= 64, l1.v /= 64;
+	l2.u /= 64, l2.v /= 64;
+	l3.u /= 64, l3.v /= 64;
+	l4.u /= 64, l4.v /= 64;
+	// right face uv: 282,821  744,821  268,1009  758,1009
+	Vertex r1 = { botw + ox, both + oy,   0 + oz, 1, 268, 1009}; // front bot
+	Vertex r2 = { topw + ox, toph + oy,   0 + oz, 1, 282, 821};  // front top
+	Vertex r3 = { botw + ox, both + oy, len + oz, 1, 758, 1009}; // far bot
+	Vertex r4 = { topw + ox, toph + oy, len + oz, 1, 744, 821};  // far top
+	r1.u /= 64, r1.v /= 64;
+	r2.u /= 64, r2.v /= 64;
+	r3.u /= 64, r3.v /= 64;
+	r4.u /= 64, r4.v /= 64;
+	return {a,b,d,a,d,c, t1,t2,t4,t1,t4,t3, l1,l2,l4,l1,l4,l3, r1,r2,r4,r1,r4,r3};
+}
+
+void genmodel(int& n_triangle, Vertex*& vertices)
+{
+	// an example model (cornell box)
+	n_triangle = 0;
+	std::vector<Vertex> res;
+	auto add = [&](std::vector<Vertex> v) {
+		if (v.size()%3!=0) throw "invalid model";
+		n_triangle += v.size()/3;
+		for (auto a: v) res.push_back(a);
+	};
+
+	for (int i=0; i<60; i+=8) {
+		add(genrail(0,0,i));
+		add(genrail(-2,0,i));
+		add(genrail(2,0,i));
+	}
+	add(gencar(-2,0,-5.5));
+	add(gencar(-2,0,0));
+	add(gencar(-2,0,5.5));
+	add(gencar(2,0,11));
+	add(gencar(2,0,16.5));
+	add(gencar(0,0,20));
+	add(gencar(0,0,25.5));
+
+	vertices = new Vertex[n_triangle*3];
+	for (int i=0; i<n_triangle*3; ++i)
+		vertices[i] = res[i];
+}
---
+++ tests/3D-int16-prototype/src/render.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <functional>
+#include <map>
+#include "data_types.hpp"
+#include "lib/consolelog.hpp"
+
+std::map<std::string, std::pair<real, real>> rec;
+
+#define record(x) {rec[#x].first=min(rec[#x].first,(x));rec[#x].second=max(rec[#x].second,(x));}
+
+Vertex perVertex(mat4 in_view, Vertex in)
+{
+
+	Vertex out;
+	out.x = in_view[0][0] * in.x + in_view[0][1] * in.y + in_view[0][2] * in.z + in_view[0][3];
+	out.y = in_view[1][0] * in.x + in_view[1][1] * in.y + in_view[1][2] * in.z + in_view[1][3];
+	out.z = in_view[2][0] * in.x + in_view[2][1] * in.y + in_view[2][2] * in.z + in_view[2][3];
+	out.w = in_view[3][0] * in.x + in_view[3][1] * in.y + in_view[3][2] * in.z + in_view[3][3];
+	// perspective division
+	out.w = real(1) / out.w;
+	out.x *= out.w;
+	out.y *= out.w;
+	out.z *= out.w;
+	// viewport
+	out.x = (out.x+1) * 10;
+	out.y = (-out.y+1) * 7.5;
+	out.z = (out.z+1) * 0.5;
+	// texture coord
+	out.u = in.u;
+	out.v = in.v;
+	return out;
+}
+
+// perspective interpolation https://stackoverflow.com/a/24460895/7884249
+void render(const mat4& in_view, int in_ntrig, Vertex* in_trigs, char* out_color, std::function<Color(real,real)> getTexture)
+{
+	// screen resolution
+	const int w = 640;
+	const int h = 480;
+	real zbuffer[w][h];
+	Color colorbuffer[w][h];
+	// clear buffers
+	for (int i=0; i<w; ++i)
+	for (int j=0; j<h; ++j) {
+		zbuffer[i][j] = 100; // max value possible
+		colorbuffer[i][j].r = 0;
+		colorbuffer[i][j].g = 0;
+		colorbuffer[i][j].b = 0;
+	}
+	// loop over all triangles
+	for (int i=0; i<in_ntrig; ++i)
+	{
+		// fetch triangle
+		Vertex v1 = in_trigs[3*i+0];
+		Vertex v2 = in_trigs[3*i+1];
+		Vertex v3 = in_trigs[3*i+2];
+		// apply transformation
+		Vertex sv1 = perVertex(in_view, v1);
+		Vertex sv2 = perVertex(in_view, v2);
+		Vertex sv3 = perVertex(in_view, v3);
+		// backface culling: none
+		// precompute barycentric coefficients
+		const real denom = real(1) / ((sv1.x-sv3.x) * (sv2.y-sv1.y) - (sv1.x-sv2.x) * (sv3.y-sv1.y));
+		const vec3 bary_x = vec3( denom * (sv2.y - sv3.y), denom * (sv3.y - sv1.y), denom * (sv1.y - sv2.y) );
+		const vec3 bary_y = vec3( denom * (sv3.x - sv2.x), denom * (sv1.x - sv3.x), denom * (sv2.x - sv1.x) );
+		const vec3 bary_c = vec3(
+	        denom * (sv2.x*sv3.y - sv3.x*sv2.y),
+	       	denom * (sv3.x*sv1.y - sv1.x*sv3.y),
+	        denom * (sv1.x*sv2.y - sv2.x*sv1.y)
+	    ); // this must be stored in higher precision
+		// calculate bounding box
+		int lbound = max(0, min(intfloor(32*sv1.x), min(intfloor(32*sv2.x), intfloor(32*sv3.x))));
+		int rbound = min(w, max(intfloor(32*sv1.x), max(intfloor(32*sv2.x), intfloor(32*sv3.x)))+1);
+		int ubound = max(0, min(intfloor(32*sv1.y), min(intfloor(32*sv2.y), intfloor(32*sv3.y))));
+		int dbound = min(h, max(intfloor(32*sv1.y), max(intfloor(32*sv2.y), intfloor(32*sv3.y)))+1);
+		// loop over all pixels in bounding box
+		for (int i=lbound; i<rbound; ++i)
+		for (int j=ubound; j<dbound; ++j)
+		{
+			real x = (real)i/32;
+			real y = (real)j/32;
+			// barycentric coordinate
+        	real bary1 = x * bary_x.x + y * bary_y.x + bary_c.x;
+        	real bary2 = x * bary_x.y + y * bary_y.y + bary_c.y;
+        	real bary3 = x * bary_x.z + y * bary_y.z + bary_c.z;
+			// determine if pixel is inside triangle
+        	bool inside = bary1>=0 && bary2>=0 && bary3>=0;
+			// perspective interpolation
+			real z = bary1 * sv1.z + bary2 * sv2.z + bary3 * sv3.z;
+			real w = bary1 * sv1.w + bary2 * sv2.w + bary3 * sv3.w;
+			// near/far plane clip
+			bool insideclip = z>=0 /*&& z<=1*/;
+			// convert to perspective correct (clip-space) barycentric
+			real inv_w = 1/w;
+			real psp1 = inv_w * bary1 * sv1.w;
+			real psp2 = inv_w * bary2 * sv2.w;
+			real psp3 = inv_w * bary3 * sv3.w;
+			real u = psp1 * sv1.u + psp2 * sv2.u + psp3 * sv3.u;
+			real v = psp1 * sv1.v + psp2 * sv2.v + psp3 * sv3.v;
+			// check depth buffer
+			bool overwrite = zbuffer[i][j] > z;
+			// write color
+			if (inside && overwrite && insideclip) {
+				colorbuffer[i][j] = getTexture(u,v);
+				zbuffer[i][j] = z;
+			}
+		}
+	}
+	for (int i=0; i<w; ++i)
+	for (int j=0; j<h; ++j)
+		memcpy(out_color+(i+j*w)*3, colorbuffer[i]+j, 3);
+
+	// print stats
+	// for (auto p: rec)
+	// 	console.log(p.first, ' ', p.second.first, '~', p.second.second);
+}
