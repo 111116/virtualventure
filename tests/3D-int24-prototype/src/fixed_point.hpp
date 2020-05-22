350c9b33aec02935b95a95f2ec89c21ff6df7452
bilibili <fenggaobuke@gmail.com>
2020-05-22 20:33:16 +0800

    implement newton division

--- tests/3D-int24-prototype/src/fixed_point.hpp
+++ tests/3D-int24-prototype/src/fixed_point.hpp
@@ -31,7 +31,24 @@ short inv12(short x)
 
 fixed inv(fixed x)
 {
-	return ((1<<(2*trail))/(x>>8))<<8;
+	int a = x>>8;
+	if (a==0) return -2147483648;
+	int xref = (1<<(2*trail))/a;
+	// return ((1<<(2*trail))/(x>>8))<<8;
+	int a1 = ((std::abs(a)<32768?a>>4:a>>13)<<13)+(1<<12);
+	int x1 = (1<<(2*trail))/a1;
+	if (std::abs(a)<32768) x1<<=9;
+	int x2 = 2*x1 - (((long long)a*x1*x1)>>(2*trail));
+	int x3 = 2*x2 - (((long long)a*x2*x2)>>(2*trail));
+	int x4 = 2*x3 - (((long long)a*x3*x3)>>(2*trail));
+	// console.log(1<<(2*trail),a1,x1,x2);
+	// int b2 = (1<<(2*trail))/a2;
+	// int b = b1 + (a-a1)*(b2-b1)/(a2-a1);
+	if (std::abs(x2-xref) > 1000) {
+		console.log(1<<(2*trail),a,x1,x2, xref);
+		console.log(x1-xref, x2-xref, "!", a);
+	}
+	return xref<<8;
 }
 
 float errinv(float x)
